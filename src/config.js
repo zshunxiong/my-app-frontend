10d75872efb39d31af6dae2368ea6cac6c362cbf
teoh1 <teoh@program.com.tw>
2020-05-13 10:21:22 +0800

    change config url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000';
-// const API_URL = 'https://my-app-backend-api.herokuapp.com';
+//const API_URL = 'http://localhost:5000';
+const API_URL = 'https://my-app-backend-api.herokuapp.com';
 
 export { API_URL };
